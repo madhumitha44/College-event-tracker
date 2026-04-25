a949059221ecedaa86a7eaaad5a86aacaadb6cdc
Madhumitha Shanmuganathan <madhumitha.shanmuganathan@gmail.com>
2026-04-25 10:08:03 +0530

    update server

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-Set-Content server.js 'const express = require("express");
-const cors = require("cors");
-const path = require("path");
-const apiRoutes = require("./routes/api");
-require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const apiRoutes = require('./routes/api');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
-app.use("/api", apiRoutes);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api', apiRoutes);
 
-app.get("/health", (req, res) => {
-    res.send("College Event Backend is running");
+app.get('/health', (req, res) => {
+    res.send('College Event Backend is running');
 });
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
-});'
+});
